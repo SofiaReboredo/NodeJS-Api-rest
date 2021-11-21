fb9f15e11ac67961106ceeafd5c53a70afb6a282
martinquesta <martinquesta@gmail.com>
2021-11-21 19:44:40 -0300

    actuadores y su model

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 // Instancio express
 const express = require('express');
 
-<<<<<<< HEAD
-// Conexion a mongoose (MongoDb)
-const mongoose = require('mongoose');
-const mongooseMiddleware = require('./middleware/dbConection');
-
-// Requiero dotenv para configurar variables de entorno 
-require('dotenv').config();
-
 // Importo los models
 require('./models/SensorModel');
-require('./models/UsuarioModel');
 require('./models/ActuadorModel');
-
-=======
-// Importo los models
-require('./models/SensorModel');
 require('./models/UsuarioModel');
 require('./middleware/database')
->>>>>>> 3985127fc8f5cb9a08daf978870bbfa73bc85682
+/////require('./middleware/dbConection');
 
 // Importo rutas
 const authRoutes = require('./routes/auth');
@@ -31,11 +18,6 @@ const app = express();
 
 // Middleware para reconocer el objeto de la petición entrante como JSON
 app.use(express.json({ extended: false }));
-<<<<<<< HEAD
-//Middleware para DB
-app.use(mongooseMiddleware);
-=======
->>>>>>> 3985127fc8f5cb9a08daf978870bbfa73bc85682
 
 // Defino el puerto que va a escuchar el servidor.
 const port = process.env.PORT || 3000;
